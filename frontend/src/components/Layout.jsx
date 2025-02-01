2e82aa4d33b1b7bb41038b72dd8b229f2f29fa22
Naseeb Ahmed <naseeb.devpro@gmail.com>
2025-02-01 10:15:48 +0500

    scroll to top function

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -2,10 +2,12 @@ import { Outlet } from 'react-router-dom';
 import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import ScrollToTop from './ScrollToTop';
 
 function Layout() {
   return (
     <div className="min-h-screen flex flex-col">
+      <ScrollToTop />
       <Navbar />
       <main className="flex-grow container mx-auto px-4 py-8">
         <Outlet />
